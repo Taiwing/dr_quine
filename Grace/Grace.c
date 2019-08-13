03a5ddba9f984e355b68ce4c6d32d60709bb4824
Taiwing <yoyo.fofo47@gmail.com>
2019-08-13 16:20:34 +0200

    Remove the main function (use _start instead)
    The program must then be compiled with the flag -nostartfiles.

--- Grace/Grace.c
+++ Grace/Grace.c
@@ -1,6 +1,7 @@
-#define TEXT char*t[]={"#define TEXT char*t[]={","0}","#define PROGRAM int main(void){FILE*fp;if(!(fp=fopen(t[8],t[9])))return(1);char**w=t;fprintf(fp,t[0]);while(*w)fprintf(fp,t[6],34,*w++,34);fprintf(fp,t[7],t[1],10);w=t+2;while(*w)fprintf(fp,t[7],*w++,10);return(0);}","#include <stdio.h>","/*","strings:","%c%s%c,","%s%c","Grace_kid.c","w","*/","TEXT;","PROGRAM",0}
-#define PROGRAM int main(void){FILE*fp;if(!(fp=fopen(t[8],t[9])))return(1);char**w=t;fprintf(fp,t[0]);while(*w)fprintf(fp,t[6],34,*w++,34);fprintf(fp,t[7],t[1],10);w=t+2;while(*w)fprintf(fp,t[7],*w++,10);return(0);}
+#define TEXT char*t[]={"#define TEXT char*t[]={","0}","#define PROGRAM int print_program(void){FILE*fp;if(!(fp=fopen(t[9],t[10])))return(1);char**w=t;fprintf(fp,t[0]);while(*w)fprintf(fp,t[7],34,*w++,34);fprintf(fp,t[8],t[1],10);w=t+2;while(*w)fprintf(fp,t[8],*w++,10);return(0);} void _start(void){print_program();exit(0);}","#include <stdio.h>","#include <stdlib.h>","/*","strings:","%c%s%c,","%s%c","Grace_kid.c","w","*/","TEXT;","PROGRAM",0}
+#define PROGRAM int print_program(void){FILE*fp;if(!(fp=fopen(t[9],t[10])))return(1);char**w=t;fprintf(fp,t[0]);while(*w)fprintf(fp,t[7],34,*w++,34);fprintf(fp,t[8],t[1],10);w=t+2;while(*w)fprintf(fp,t[8],*w++,10);return(0);} void _start(void){print_program();exit(0);}
 #include <stdio.h>
+#include <stdlib.h>
 /*
 strings:
 %c%s%c,
