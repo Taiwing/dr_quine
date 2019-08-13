410752cd536a8f67b22b124acdbd5fabc68de32c
Taiwing <yoyo.fofo47@gmail.com>
2019-08-13 15:42:58 +0200

    Place the global array in a define

--- Grace/Grace.c
+++ Grace/Grace.c
@@ -1,92 +1,12 @@
-char*src[]={
-"char*src[]={",
-"0};",
-"/*",
-"strings: (lol)",
-"Grace_kid.c",
-"w",
-"*/",
-"#include <stdio.h>",
-"#include <string.h>",
-"#define MAIN int main(){return pseudo_main();}",
-"",
-"void	fputendl(char *s, FILE *fp)",
-"{",
-"	fputs(s, fp);",
-"	fputc(10, fp);",
-"}",
-"",
-"void	quotify(char *s, FILE *fp)",
-"{",
-"	fputc(34, fp);",
-"	fputs(s, fp);",
-"	fputc(34, fp);",
-"	fputc(44, fp);",
-"	fputc(10, fp);",
-"};",
-"",
-"void print_src(char**ptr, void(*f)(), FILE *fp)",
-"{",
-"	while(*ptr)",
-"		f(*ptr++, fp);",
-"}",
-"",
-"int pseudo_main()",
-"{",
-"	FILE	*fp;",
-"",
-"	if (!(fp = fopen(src[4], src[5])))",
-"		return (1);",
-"	fputendl(*src, fp);",
-"	print_src(src, quotify, fp);",
-"	print_src(src + 1, fputendl, fp);",
-"	fclose(fp);",
-"	return (0);",
-"}",
-"",
-"MAIN",
-0};
+#define TEXT char*t[]={"#define TEXT char*t[]={","0}","#define PROGRAM int main(void){FILE*fp;if(!(fp=fopen(t[8],t[9])))return(1);char**w=t;fprintf(fp,t[0]);while(*w)fprintf(fp,t[6],34,*w++,34);fprintf(fp,t[7],t[1],10);w=t+2;while(*w)fprintf(fp,t[7],*w++,10);return(0);}","#include <stdio.h>","/*","strings:","%c%s%c,","%s%c","Grace_kid.c","w","*/","TEXT;","PROGRAM",0}
+#define PROGRAM int main(void){FILE*fp;if(!(fp=fopen(t[8],t[9])))return(1);char**w=t;fprintf(fp,t[0]);while(*w)fprintf(fp,t[6],34,*w++,34);fprintf(fp,t[7],t[1],10);w=t+2;while(*w)fprintf(fp,t[7],*w++,10);return(0);}
+#include <stdio.h>
 /*
-strings: (lol)
+strings:
+%c%s%c,
+%s%c
 Grace_kid.c
 w
 */
-#include <stdio.h>
-#include <string.h>
-#define MAIN int main(){return pseudo_main();}
-
-void	fputendl(char *s, FILE *fp)
-{
-	fputs(s, fp);
-	fputc(10, fp);
-}
-
-void	quotify(char *s, FILE *fp)
-{
-	fputc(34, fp);
-	fputs(s, fp);
-	fputc(34, fp);
-	fputc(44, fp);
-	fputc(10, fp);
-};
-
-void print_src(char**ptr, void(*f)(), FILE *fp)
-{
-	while(*ptr)
-		f(*ptr++, fp);
-}
-
-int pseudo_main()
-{
-	FILE	*fp;
-
-	if (!(fp = fopen(src[4], src[5])))
-		return (1);
-	fputendl(*src, fp);
-	print_src(src, quotify, fp);
-	print_src(src + 1, fputendl, fp);
-	fclose(fp);
-	return (0);
-}
-
-MAIN
+TEXT;
+PROGRAM
