5c43e8d0c2b703defe08a0513b383916160e5d06
Taiwing <yoyo.fofo47@gmail.com>
2019-08-13 16:49:47 +0200

    Finish Grace for good, with Makefile

--- Grace/Grace.c
+++ Grace/Grace.c
@@ -1,7 +1,6 @@
-#define TEXT char*t[]={"#define TEXT char*t[]={","0}","#define PROGRAM int print_program(void){FILE*fp;if(!(fp=fopen(t[9],t[10])))return(1);char**w=t;fprintf(fp,t[0]);while(*w)fprintf(fp,t[7],34,*w++,34);fprintf(fp,t[8],t[1],10);w=t+2;while(*w)fprintf(fp,t[8],*w++,10);return(0);} void _start(void){print_program();exit(0);}","#include <stdio.h>","#include <stdlib.h>","/*","strings:","%c%s%c,","%s%c","Grace_kid.c","w","*/","TEXT;","PROGRAM",0}
-#define PROGRAM int print_program(void){FILE*fp;if(!(fp=fopen(t[9],t[10])))return(1);char**w=t;fprintf(fp,t[0]);while(*w)fprintf(fp,t[7],34,*w++,34);fprintf(fp,t[8],t[1],10);w=t+2;while(*w)fprintf(fp,t[8],*w++,10);return(0);} void _start(void){print_program();exit(0);}
-#include <stdio.h>
-#include <stdlib.h>
+#define TEXT char*t[]={"#define TEXT char*t[]={","0}","#define PROGRAM int print_program(void){FILE*fp;if(!(fp=fopen(t[8],t[9])))return(1);char**w=t;fprintf(fp,t[0]);while(*w)fprintf(fp,t[6],34,*w++,34);fprintf(fp,t[7],t[1],10);w=t+2;while(*w)fprintf(fp,t[7],*w++,10);fclose(fp);return(0);} void _start(void){print_program();exit(0);}","#define LOL_IM_A_COMPLETELY_USELESS_DEFINE","/*","strings:","%c%s%c,","%s%c","Grace_kid.c","w","*/","TEXT;","PROGRAM",0}
+#define PROGRAM int print_program(void){FILE*fp;if(!(fp=fopen(t[8],t[9])))return(1);char**w=t;fprintf(fp,t[0]);while(*w)fprintf(fp,t[6],34,*w++,34);fprintf(fp,t[7],t[1],10);w=t+2;while(*w)fprintf(fp,t[7],*w++,10);fclose(fp);return(0);} void _start(void){print_program();exit(0);}
+#define LOL_IM_A_COMPLETELY_USELESS_DEFINE
 /*
 strings:
 %c%s%c,
