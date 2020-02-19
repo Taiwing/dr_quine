4613551a4933364d6f84cd55b0cfd51032efe591
Taiwing <yoyo.fofo47@gmail.com>
2020-02-19 02:38:14 +0100

    Reduce Colleen even more

--- ColleenV2.c
+++ ColleenV2.c
@@ -1,12 +1,14 @@
 char*t[]={
 "char a[]={'c','h','a','r','*','t','[',']','=','{',0,'0','}',';',0};",
 "#include<stdio.h>/**/",
-"int q(char*s){putchar(34);printf(s);putchar(34);putchar(44);putchar(10);return 0;};",
-"void p(char**w,int(*f)()){while(*w)f(*w++);}",
+"#define w putchar",
+"int q(char*s){w(34);printf(s);w(34);w(44);w(10);return 0;};",
+"void p(char**z,int(*f)()){while(*z)f(*z++);}",
 "int main(){/**/puts(a);p(t,q);puts(a+11);p(t,puts);}",
 0};
 char a[]={'c','h','a','r','*','t','[',']','=','{',0,'0','}',';',0};
 #include<stdio.h>/**/
-int q(char*s){putchar(34);printf(s);putchar(34);putchar(44);putchar(10);return 0;};
-void p(char**w,int(*f)()){while(*w)f(*w++);}
+#define w putchar
+int q(char*s){w(34);printf(s);w(34);w(44);w(10);return 0;};
+void p(char**z,int(*f)()){while(*z)f(*z++);}
 int main(){/**/puts(a);p(t,q);puts(a+11);p(t,puts);}
