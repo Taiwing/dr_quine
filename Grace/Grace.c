b08a70ff45bd8b34f0c8db2e3425f38d65bac83d
Taiwing <yoyo.fofo47@gmail.com>
2020-02-19 04:17:47 +0100

    Add first version of Grace (with main)

---
+++ Grace/Grace.c
@@ -0,0 +1,88 @@
+char*src[]={
+"char*src[]={",
+"0};",
+"/*",
+"strings: (lol)",
+"Grace_kid.c",
+"w",
+"*/",
+"#include <stdio.h>",
+"#include <string.h>",
+"",
+"",
+"void	fputendl(char *s, FILE *fp)",
+"{",
+"	fputs(s, fp);",
+"	fputc(10, fp);",
+"}",
+"",
+"void	quotify(char *s, FILE *fp)",
+"{",
+"	fputc(34, fp);",
+"	fputs(s, fp);",
+"	fputc(34, fp);",
+"	fputc(44, fp);",
+"	fputc(10, fp);",
+"};",
+"",
+"void print_src(char**ptr, void(*f)(), FILE *fp)",
+"{",
+"	while(*ptr)",
+"		f(*ptr++, fp);",
+"}",
+"",
+"int main()",
+"{",
+"	FILE	*fp;",
+"",
+"	if (!(fp = fopen(src[4], src[5])))",
+"		return (1);",
+"	fputendl(*src, fp);",
+"	print_src(src, quotify, fp);",
+"	print_src(src + 1, fputendl, fp);",
+"	fclose(fp);",
+"	return (0);",
+"}",
+0};
+/*
+strings: (lol)
+Grace_kid.c
+w
+*/
+#include <stdio.h>
+#include <string.h>
+
+
+void	fputendl(char *s, FILE *fp)
+{
+	fputs(s, fp);
+	fputc(10, fp);
+}
+
+void	quotify(char *s, FILE *fp)
+{
+	fputc(34, fp);
+	fputs(s, fp);
+	fputc(34, fp);
+	fputc(44, fp);
+	fputc(10, fp);
+};
+
+void print_src(char**ptr, void(*f)(), FILE *fp)
+{
+	while(*ptr)
+		f(*ptr++, fp);
+}
+
+int main()
+{
+	FILE	*fp;
+
+	if (!(fp = fopen(src[4], src[5])))
+		return (1);
+	fputendl(*src, fp);
+	print_src(src, quotify, fp);
+	print_src(src + 1, fputendl, fp);
+	fclose(fp);
+	return (0);
+}
