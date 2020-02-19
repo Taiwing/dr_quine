3af21eb7fd9cfa80ae89168a983fd63672e6b807
Taiwing <yoyo.fofo47@gmail.com>
2020-02-19 02:55:49 +0100

    Put everything in the src array
    This reduces the code length once again eeeasssy

--- ColleenV2.c
+++ ColleenV2.c
@@ -1,14 +1,14 @@
 char*t[]={
-"char a[]={'c','h','a','r','*','t','[',']','=','{',0,'0','}',';',0};",
+"char*t[]={",
+"0};",
 "#include<stdio.h>/**/",
-"#define w putchar",
-"int q(char*s){w(34);printf(s);w(34);w(44);w(10);return 0;};",
+"#define W putchar",
+"int q(char*s){W(34);printf(s);W(34);W(44);W(10);return 0;};",
 "void p(char**z,int(*f)()){while(*z)f(*z++);}",
-"int main(){/**/puts(a);p(t,q);puts(a+11);p(t,puts);}",
+"int main(){/**/puts(*t);p(t,q);p(t+1,puts);}",
 0};
-char a[]={'c','h','a','r','*','t','[',']','=','{',0,'0','}',';',0};
 #include<stdio.h>/**/
-#define w putchar
-int q(char*s){w(34);printf(s);w(34);w(44);w(10);return 0;};
+#define W putchar
+int q(char*s){W(34);printf(s);W(34);W(44);W(10);return 0;};
 void p(char**z,int(*f)()){while(*z)f(*z++);}
-int main(){/**/puts(a);p(t,q);puts(a+11);p(t,puts);}
+int main(){/**/puts(*t);p(t,q);p(t+1,puts);}
