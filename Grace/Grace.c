fa93c1eaf47f5d766ee97d11734908d971e0435f
yoann foreau <yoann.foreau@ito.am>
2020-12-22 10:39:58 +0100

    Fix new trololo warning from gcc

--- Grace/Grace.c
+++ Grace/Grace.c
@@ -1,5 +1,5 @@
-#define TEXT char*t[]={"#define TEXT char*t[]={","0}","#define PROGRAM int print_program(void){FILE*fp;if(!(fp=fopen(t[8],t[9])))return(1);char**w=t;fprintf(fp,t[0]);while(*w)fprintf(fp,t[6],34,*w++,34);fprintf(fp,t[7],t[1],10);w=t+2;while(*w)fprintf(fp,t[7],*w++,10);fclose(fp);return(0);} void _start(void){print_program();exit(0);}","#define LOL_IM_A_COMPLETELY_USELESS_DEFINE","/*","strings:","%c%s%c,","%s%c","Grace_kid.c","w","*/","TEXT;","PROGRAM",0}
-#define PROGRAM int print_program(void){FILE*fp;if(!(fp=fopen(t[8],t[9])))return(1);char**w=t;fprintf(fp,t[0]);while(*w)fprintf(fp,t[6],34,*w++,34);fprintf(fp,t[7],t[1],10);w=t+2;while(*w)fprintf(fp,t[7],*w++,10);fclose(fp);return(0);} void _start(void){print_program();exit(0);}
+#define TEXT char*t[]={"#define TEXT char*t[]={","0}","#define PROGRAM int print_program(void){FILE*fp;if(!(fp=fopen(t[8],t[9])))return(1);char**w=t;fprintf(fp,t[0],0);while(*w)fprintf(fp,t[6],34,*w++,34);fprintf(fp,t[7],t[1],10);w=t+2;while(*w)fprintf(fp,t[7],*w++,10);fclose(fp);return(0);} void _start(void){print_program();exit(0);}","#define LOL_IM_A_COMPLETELY_USELESS_DEFINE","/*","strings:","%c%s%c,","%s%c","Grace_kid.c","w","*/","TEXT;","PROGRAM",0}
+#define PROGRAM int print_program(void){FILE*fp;if(!(fp=fopen(t[8],t[9])))return(1);char**w=t;fprintf(fp,t[0],0);while(*w)fprintf(fp,t[6],34,*w++,34);fprintf(fp,t[7],t[1],10);w=t+2;while(*w)fprintf(fp,t[7],*w++,10);fclose(fp);return(0);} void _start(void){print_program();exit(0);}
 #define LOL_IM_A_COMPLETELY_USELESS_DEFINE
 /*
 strings:
