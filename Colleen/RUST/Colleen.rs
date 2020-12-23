2d1b398bb5476643f9e8f5a585c6c850665b1c76
yoann foreau <yoann.foreau@ito.am>
2020-12-23 11:43:43 +0100

    Add useless comments for the subject

--- Colleen/RUST/Colleen.rs
+++ Colleen/RUST/Colleen.rs
@@ -1,7 +1,8 @@
-static SRC: [&str; 16] = [
-    "static SRC: [&str; 16] = [",
+static SRC: [&str; 18] = [
+    "static SRC: [&str; 18] = [",
     "];",
     "",
+    "/* Hey! Loook at me, I am a comment! */",
     "fn print_iterator<T: Iterator<Item = String>>(striter: T, lq: &str, rq: &str) {",
     "    for line in striter {",
     "        println!(\"{}{}{}\", lq, line, rq);",
@@ -9,6 +10,7 @@ static SRC: [&str; 16] = [
     "}",
     "",
     "fn main() {",
+    "    /* I am a super useful comment inside main */",
     "    let quoted_src_iter = SRC.iter().map(|s| s.escape_debug().to_string());",
     "    let mut raw_src_iter = SRC.iter().map(|s| String::from(*s));",
     "    println!(\"{}\", raw_src_iter.next().unwrap());",
@@ -17,6 +19,7 @@ static SRC: [&str; 16] = [
     "}",
 ];
 
+/* Hey! Loook at me, I am a comment! */
 fn print_iterator<T: Iterator<Item = String>>(striter: T, lq: &str, rq: &str) {
     for line in striter {
         println!("{}{}{}", lq, line, rq);
@@ -24,6 +27,7 @@ fn print_iterator<T: Iterator<Item = String>>(striter: T, lq: &str, rq: &str) {
 }
 
 fn main() {
+    /* I am a super useful comment inside main */
     let quoted_src_iter = SRC.iter().map(|s| s.escape_debug().to_string());
     let mut raw_src_iter = SRC.iter().map(|s| String::from(*s));
     println!("{}", raw_src_iter.next().unwrap());
