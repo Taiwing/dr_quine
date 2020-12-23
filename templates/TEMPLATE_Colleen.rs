ed34d36d7f4e59ce61c93d3ccf289408517d2610
yoann foreau <yoann.foreau@ito.am>
2020-12-23 13:15:13 +0100

    Create template for rusty Colleen

---
+++ templates/TEMPLATE_Colleen.rs
@@ -0,0 +1,15 @@
+/* Hey! Loook at me, I am a comment! */
+fn print_iterator<T: Iterator<Item = String>>(striter: T, lq: &str, rq: &str) {
+    for line in striter {
+        println!("{}{}{}", lq, line, rq);
+    }
+}
+
+fn main() {
+    /* I am a super useful comment inside main */
+    let quoted_src_iter = SRC.iter().map(|s| s.escape_debug().to_string());
+    let mut raw_src_iter = SRC.iter().map(|s| String::from(*s));
+    println!("{}", raw_src_iter.next().unwrap());
+    print_iterator(quoted_src_iter, "    \"", "\",");
+    print_iterator(raw_src_iter, "", "");
+}
