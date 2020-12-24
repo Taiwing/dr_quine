56530d3af07eac28c6e728d6d0e3899c59a11db9
yoann foreau <yoann.foreau@ito.am>
2020-12-24 08:25:28 +0100

    Remove everything not a macro or a comment

--- templates/TEMPLATE_Grace.rs
+++ templates/TEMPLATE_Grace.rs
@@ -4,26 +4,34 @@ macro_rules! format_as_bytes {
     }
 }
 
-/* Hey! Loook at me, I am a comment! */
-macro_rules! print_iterator {
-    ($file:ident, $str_iter:ident, $lq:expr, $rq:expr) => {
-        for line in $str_iter {
-            $file.write_all(format_as_bytes!("{}{}{}\n", $lq, line, $rq))?;
-        }
-    }
-}
+/*
+    Hey! Loook at me, I am a comment!
+*/
 macro_rules! start_program {
     () => {
+        src_macro!();
         use std::fs::File;
         use std::io::prelude::*;
-        
+
+        fn print_iterator<T: Iterator<Item = String>>(
+            file: &mut File,
+            striter: T,
+            lq: &str,
+            rq: &str
+        ) -> std::io::Result<()> {
+            for line in striter {
+                file.write_all(format_as_bytes!("{}{}{}\n", lq, line, rq))?;
+            }
+            Ok(())
+        }
+
         fn main() -> std::io::Result<()> {
             let mut file = File::create("Grace_kid.rs")?;
             let quoted = SRC.iter().map(|s| s.escape_debug().to_string());
             let mut raw = SRC.iter().map(|s| String::from(*s));
             file.write_all(format_as_bytes!("{}\n", raw.next().unwrap()))?;
-            print_iterator!(file, quoted, "    \"", "\",");
-            print_iterator!(file, raw, "", "");
+            print_iterator(&mut file, quoted, "    \"", "\",")?;
+            print_iterator(&mut file, raw, "", "")?;
             Ok(())
         }
     }
