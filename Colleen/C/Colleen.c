99bcbe3fa62d1ee53b13e612ebe00a634e3b9cdf
yoann foreau <yoann.foreau@ito.am>
2022-04-11 17:39:18 +0200

    feat: shortify Colleen C further

--- Colleen/C/Colleen.c
+++ Colleen/C/Colleen.c
@@ -1,30 +1,12 @@
 char*t[]={
 "char*t[]={",
 "0};",
-"/*",
-"	Colleen.c",
-"*/",
-"#include<stdio.h>",
 "#define W putchar",
 "int q(char*s){W(34);printf(s,0);W(34);W(44);W(10);return 0;};",
 "void p(char**z,int(*f)()){while(*z)f(*z++);}",
-"int main(){",
-"	/*",
-"		mandatory 3 lines comment totally ruining my one-liner main :(",
-"	*/",
-"	puts(*t);p(t,q);p(t+1,puts);",
-"}",
+"int main(){puts(*t);p(t,q);p(t+1,puts);}",
 0};
-/*
-	Colleen.c
-*/
-#include<stdio.h>
 #define W putchar
 int q(char*s){W(34);printf(s,0);W(34);W(44);W(10);return 0;};
 void p(char**z,int(*f)()){while(*z)f(*z++);}
-int main(){
-	/*
-		mandatory 3 lines comment totally ruining my one-liner main :(
-	*/
-	puts(*t);p(t,q);p(t+1,puts);
-}
+int main(){puts(*t);p(t,q);p(t+1,puts);}
--- templates/TEMPLATE_Colleen.c
+++ templates/TEMPLATE_Colleen.c
@@ -1,5 +1,4 @@
-#include<stdio.h>/*_*/
 #define W putchar
 int q(char*s){W(34);printf(s,0);W(34);W(44);W(10);return 0;};
 void p(char**z,int(*f)()){while(*z)f(*z++);}
-int main(){/**/puts(*t);p(t,q);p(t+1,puts);}
+int main(){puts(*t);p(t,q);p(t+1,puts);}
