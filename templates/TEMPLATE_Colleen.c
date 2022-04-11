e63298f938361d5a322f056e21dd21d12e971d72
yoann foreau <yoann.foreau@ito.am>
2022-04-11 17:43:27 +0200

    feat: shortify it even further

--- templates/TEMPLATE_Colleen.c
+++ templates/TEMPLATE_Colleen.c
@@ -1,4 +1,4 @@
 #define W putchar
-int q(char*s){W(34);printf(s,0);W(34);W(44);W(10);return 0;};
-void p(char**z,int(*f)()){while(*z)f(*z++);}
+int q(char*s){W(34);printf(s,0);W(34);W(44);W(10);};
+int p(char**z,int(*f)()){while(*z)f(*z++);}
 int main(){puts(*t);p(t,q);p(t+1,puts);}
